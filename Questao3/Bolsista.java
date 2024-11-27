2db21d03406b7318b5ff22b687641b89f43c287c
Moisés Madeira <msalcmd@gmail.com>
2024-11-26 21:46:22 -0300

    Removi pastas de questões

--- Questao3/Bolsista.java
+++
@@ -1,8 +0,0 @@
-package atividadefinal;
-
-public class Bolsista extends Aluno{
-    public void pagarMensalidade() {
-        super.pagarMensalidade();
-        System.out.println("\tComo é um aluno bolsista, vai ter o desconto devido.");
-    }
-}
